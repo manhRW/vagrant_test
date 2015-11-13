492a0acf97299d459f02eb59c9c9f9bd6754f14d
manh <manh@PC243.local>
2015-11-13 18:05:39 +0900

    add first sleep time

--- sh/test.sh
+++ sh/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-su - vagrant
+start_time=$(date +%s)
+check_time=$(date -d "23:59:59" +%s)
+sleep_time=$(( check_time - start_time ))
+sleep $sleep_time
+
 while :
 do  
     sudo tac /var/log/httpd/access_log > log.txt
@@ -13,26 +17,26 @@ do
         post_register=0
         get_homepage=0
         post_homepage=0
-        date=`date +"%d"`
-
+        day=`date +"%d"`
+        date=`date +"%d/%b/%Y"`
         while IFS= read -r line 
         do
-            if [[ $line != *"$date"* ]]
+            if [[ $line != *"$day"* ]]
             then
                 break
             fi
 
             case "$line" in
                 *GET*login*) 
-              	    get_login=`expr $get_login + 1`
-		            ;;
-	            *POST*login*)
+                    get_login=`expr $get_login + 1`
+                            ;;
+                    *POST*login*)
                     post_login=`expr $post_login + 1`
                     ;;
-		        *GET*register*)
+                        *GET*register*)
                     get_register=`expr $get_register + 1`
                     ;;
-		        *POST*register*)
+                        *POST*register*)
                     post_register=`expr $post_register + 1`
                     ;;
                 *GET*homepage*)
@@ -41,11 +45,11 @@ do
                 *POST*homepage*)
                     post_homepage=`expr $post_homepage + 1`
                     ;;
-	        esac
-        done < "$file"
-	echo -e "login[method:GET] $get_login\nlogin[method:POST] $post_login\nregister[method:GET] $get_register\nregister[method:POST] $post_register\nhomepage[method:GET] $get_homepage\nhomepage[method:POST] $post_homepage" | mail -s "{log}$date" manh@realworld.jp
+                esac
+        done < "$file"        
+        echo -e "login[method:GET] $get_login\nlogin[method:POST] $post_login\nregister[method:GET] $get_register\nregister[method:POST] $post_register\nhomepage[method:GET] $get_homepage\nhomepage[method:POST] $post_homepage" | mail -s "{log}$date" manh@realworld.jp
     echo "Mail sent"
     rm log.txt
-	sleep 24h
+        sleep 24h
     fi
 done
